f117c55f72f13f6755412e539311b29db4ed31ab
Przemek B <pprzemek89@gmail.com>
2024-02-22 13:24:51 +0100

    fgfg

--- src/Biblioteka.java
+++ src/Biblioteka.java
@@ -5,8 +5,10 @@ import wypozyczenie.Egzemplarz;
 import wypozyczenie.Wypozyczenie;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Biblioteka {
+    Scanner scanner = new Scanner(System.in);
     ArrayList<Ksiazka> ksiazki = new ArrayList<>();
     ArrayList<Egzemplarz> egzemplarze = new ArrayList<>();
     ArrayList<Wypozyczenie> wypozyczenia = new ArrayList<>();
@@ -39,7 +41,43 @@ public class Biblioteka {
                     && egzemplarz.getStan() == Egzemplarz.Stan.WOLNY) return egzemplarz;
         }
         return null;
+    }
+    public void dodajKsiazke(){
+        System.out.println("Podaj tytuł: ");
+        String tytul = scanner.nextLine();
+        System.out.println("Podaj gatunek (k -> kryminal; p -> populano-naukowe; f -> fantastyka; b -> biografia): ");
+        Gatunek gatunek = switch (scanner.nextLine()){
+            case "p" -> Gatunek.POPULARNO_NAUKOWE;
+            case "f" -> Gatunek.FANTASTYKA;
+            case "b" -> Gatunek.BIOGRAFIA;
+            default -> Gatunek.KRYMINAL;
+        };
 
+        System.out.println("Podaj autora: ");
+        Autor autor = stworzAutora();
+        System.out.println("Podaj rok wydania: ");
+        int rokWydania = Integer.parseInt(scanner.nextLine());
+        Ksiazka nowaKsiazka = new Ksiazka(tytul, gatunek, autor, rokWydania);
+        for (Ksiazka k :
+                ksiazki) {
+            if (k.equals(nowaKsiazka)){
+                System.out.println("Ksiazka juz istnieje");
+                return;
+            }
+        }
+        ksiazki.add(nowaKsiazka);
+
+    }
+    public Autor stworzAutora(){
+        System.out.println("Podaj imie autora: ");
+        String imie = scanner.nextLine();
+        System.out.println("Podaj nazwisko autora: ");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Podaj rok urodzenia: ");
+        int rokUrodzenia = Integer.parseInt(scanner.nextLine());
+        System.out.println("Podaj rok smierci (jesli autor zyje wpisz 0): ");
+        int rokSmierci = Integer.parseInt(scanner.nextLine());
+        return new Autor(imie, nazwisko, rokUrodzenia, rokSmierci);
     }
 
 }
